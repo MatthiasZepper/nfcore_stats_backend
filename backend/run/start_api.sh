232394831833b06424e9c0fafc5b2ccc4a477faf
Matthias Zepper <MatthiasZepper@users.noreply.github.com>
2022-07-22 17:58:52 +0200

    'poetry show' for some reason already creates the virtual environment and then uvicorn and celery are not found.

--- backend/run/start_api.sh
+++ backend/run/start_api.sh
@@ -4,4 +4,4 @@ set -o errexit
 set -eo pipefail
 set -o nounset
 
-/usr/bin/uvicorn mnt.backend.api.main:app --host 0.0.0.0 --port 8000 --reload
+/usr/local/bin/uvicorn mnt.backend.api.main:app --host 0.0.0.0 --port 8000 --reload
--- backend/run/start_scheduler.sh
+++ backend/run/start_scheduler.sh
@@ -7,4 +7,4 @@ set -o nounset
 mkdir -p /var/run/celery /var/log/celery
 chown -R nobody:nogroup /var/run/celery /var/log/celery
 
-/usr/bin/celery --app=mnt.backend.api.tasks worker --beat -l info -Q main-queue -c 1 --uid=nobody --gid=nogroup
+/usr/local/bin/celery --app=mnt.backend.api.tasks worker --beat -l info -Q main-queue -c 1 --uid=nobody --gid=nogroup
